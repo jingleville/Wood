f2021d54c67444bdf1566f41b6be05c31cc7bb59
jingleville <andrewgavrick@yandex.ru>
2023-05-26 13:59:40 +0300

    Updated carousel, auto collecting files from actual folder, auto resize

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,9 +6,17 @@ class PagesController < ApplicationController
   end
 
   def alt2
+    @images = {}
+    img_list = Dir.glob("app/assets/images/Product 1/*.JPG")
+    img_list.map { |image| image['app/assets/images/'] = ''}
+    @images["Product 1"] = img_list
+    @images["Product 2"] = Dir.glob("app/assets/images/Product 2/*.JPG")
   end
 
   def alt3
+
+
+
   end
 
 end
