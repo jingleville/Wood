f523ba78fecfcb60e96e6a459454a6378923d16a
jingleville <andrewgavrick@yandex.ru>
2023-05-26 17:56:43 +0300

    set up carousel

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
+  before_action :imgs
+
   def main
   end
 
@@ -6,17 +8,28 @@ class PagesController < ApplicationController
   end
 
   def alt2
-    @images = {}
-    img_list = Dir.glob("app/assets/images/Product 1/*.JPG")
-    img_list.map { |image| image['app/assets/images/'] = ''}
-    @images["Product 1"] = img_list
-    @images["Product 2"] = Dir.glob("app/assets/images/Product 2/*.JPG")
   end
 
   def alt3
+  end
 
+  def alt4
+  end
 
+  def alt5
+  end
 
+  def alt6
+  end  
+
+  private
+
+  def imgs
+    @images = {}
+    img_list = Dir.glob("app/assets/images/Product 1/*.JPG")
+    img_list.map { |image| image['app/assets/images/'] = ''}
+    @images["Product_1"] = img_list
+    # @images["Product_2"] = Dir.glob("app/assets/images/Product 2/*.JPG")
   end
 
 end
